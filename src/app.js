5ab238474af9183608bf78d2e31d29e832e3ed78
Ashmide <ashmidejnbaptiste@gmail.com>
2025-10-02 21:59:25 -0300

    aaaaaa

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ const app = express();
 
 // Para iniciar sua aplicação digite nodemon server.js
 console.log('Teste da prova !');
-console.log('Teste da prova !a');
-
+console.log('Teste da prova !');
+console.log('Teste da prova !');
 module.exports = app;
